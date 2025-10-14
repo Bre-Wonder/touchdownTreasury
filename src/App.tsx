df9aa20c67bf26d052a712c32aab84673255b0f8
Bre-Wonder <breannasteward503@gmail.com>
2025-10-14 16:06:36 -0700

    home typescript file created

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import ProductGrid from './components/ProductGrid';
+import Footer from './components/Footer';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <Header />
+      <main>
+        <Hero />
+        <ProductGrid />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
+
