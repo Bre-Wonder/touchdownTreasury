580559e252b0ad38b49a6a7a48e1d30c979228d6
Bre-Wonder <breannasteward503@gmail.com>
2025-10-15 15:34:11 -0700

    created routes for home page

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './Hero';
+import ProductGrid from './ProductGrid';
+
+const Home: React.FC = () => {
+  return (
+    <main>
+      <Hero />
+      <ProductGrid />
+    </main>
+  );
+};
+
+export default Home;
