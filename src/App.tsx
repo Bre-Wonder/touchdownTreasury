89e10d20b2491755eff6e1e3e3aeadeba2a9f057
Bre-Wonder <breannasteward503@gmail.com>
2025-10-15 15:33:33 -0700

    added React routing for user to navigate to different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Hero from './components/Hero';
-import ProductGrid from './components/ProductGrid';
+import Home from './components/Home';
+import Draft from './components/Draft';
+import Playoffs from './components/Playoffs';
 import Footer from './components/Footer';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="app">
-      <Header />
-      <main>
-        <Hero />
-        <ProductGrid />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/draft" element={<Draft />} />
+          <Route path="/playoffs" element={<Playoffs />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
