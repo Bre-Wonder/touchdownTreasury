28297bb4961929dacb2b29a487399b7183608c41
Bre-Wonder <breannasteward503@gmail.com>
2025-10-30 17:12:29 -0700

    added routing for model

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Draft from './components/Draft';
 import Playoffs from './components/Playoffs';
 import Footer from './components/Footer';
+import GoDucksModal from './components/GoDucksModal';
 import './App.css';
 
 const App: React.FC = () => {
+  const [showGoDucksModal, setShowGoDucksModal] = useState(true);
+
+  const handleAgree = () => {
+    setShowGoDucksModal(false);
+  };
+
   return (
     <Router>
       <div className="app">
+        <GoDucksModal isOpen={showGoDucksModal} onAgree={handleAgree} />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
