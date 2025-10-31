48ed54ba4a520c03ce06489360e8a7da5a315ae4
Bre-Wonder <breannasteward503@gmail.com>
2025-10-30 17:11:09 -0700

    added model with styling

---
+++ src/components/GoDucksModal.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './GoDucksModal.css';
+
+interface GoDucksModalProps {
+  isOpen: boolean;
+  onAgree: () => void;
+}
+
+const GoDucksModal: React.FC<GoDucksModalProps> = ({ isOpen, onAgree }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="go-ducks-modal-overlay">
+      <div className="go-ducks-modal">
+        <div className="go-ducks-modal-content">
+          <img 
+            src="/images/ducks.png" 
+            alt="Ducks" 
+            className="go-ducks-image"
+          />
+          <h2 className="go-ducks-title">Go Ducks!</h2>
+          <button 
+            className="go-ducks-agree-btn" 
+            onClick={onAgree}
+          >
+            I Agree
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GoDucksModal;
+
